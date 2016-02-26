0a6db82cfb65f49bdfccb41fccf36fd18c603d3e
Satish Chandra Panigrahi <satishPanigrahi@satishs-macbook-pro.local>
2016-02-26 15:18:31 -0400

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Satish Chandra Panigrahi on 2016-02-26.
+//  Copyright © 2016 Satish Chandra Panigrahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
