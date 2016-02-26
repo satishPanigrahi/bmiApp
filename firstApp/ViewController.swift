69f50f044d8952d290f2801439ba28dd9254be32
Satish Chandra Panigrahi <satishPanigrahi@satishs-macbook-pro.local>
2016-02-26 17:41:41 -0400

    Changed input to decimal and calculate vmi

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -22,12 +22,27 @@ class ViewController: UIViewController {
     
     func promptForMsg(){
         let ac = UIAlertController(title: "Enter Message", message: nil, preferredStyle: .Alert)
-        ac.addTextFieldWithConfigurationHandler(nil)
+        ac.addTextFieldWithConfigurationHandler{(textField: UITextField!) in
+        textField.keyboardType = UIKeyboardType.DecimalPad
+        textField.placeholder = "Mass (kg)"
+        
+        }
+        ac.addTextFieldWithConfigurationHandler{(textField: UITextField!) in
+            textField.keyboardType = UIKeyboardType.DecimalPad
+            textField.placeholder = "Height (m)"
+        }
+
         
         let submitAction = UIAlertAction(title: "Submit", style: .Default) {
             [unowned self, ac] (action: UIAlertAction) in
-            let answer = ac.textFields![0]
-            self.labelText.text = answer.text
+            let weight = Float(ac.textFields![0].text!)
+            // check and error message
+            let height = Float(ac.textFields![1].text!)
+            // check and error message
+            let bmi = weight!/(height! * height!)
+            // some thing like try catch
+
+            self.labelText.text = String(bmi)
             
         }
         
